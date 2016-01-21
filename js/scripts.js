7acc44f09b6074246eb171d1a4daddef63801411
Nicholas Jensen-Hay <nicholasjensenhay@gmail.com>
2016-01-21 10:04:22 -0800

    make countUpBy count up to the smaller number when both numbers are negative

--- js/scripts.js
+++ js/scripts.js
@@ -12,6 +12,9 @@ var countUpBy = function(countBy, countTo) {
       }
     }
   } else {
+    if (countBy < countTo) {
+      countBy = [countTo, countTo = countBy][0];
+    }
     for (var index = countBy; index >= countTo; index += countBy) {
       if(index>=countTo) {
         output.push(index);
