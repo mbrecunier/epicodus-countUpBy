cc2e0947ee970b6caa60cc492043bd2c5b98ea3e
Nicholas Jensen-Hay <nicholasjensenhay@gmail.com>
2016-01-21 09:09:03 -0800

    write spec for handling string input in countUpBy

--- spec/specs.js
+++ spec/specs.js
@@ -1,9 +1,13 @@
 describe('countUpBy', function() {
   it('counts up by a number provided to another number provided', function() {
-    expect(countUpBy(5, 30)).to.equal('5, 10, 15, 20, 25, 30')
+    expect(countUpBy(5, 30)).to.equal('5, 10, 15, 20, 25, 30');
   });
 
   it('will always count to the large input by the smaller input', function() {
-    expect(countUpBy(35, 5)).to.equal('5, 10, 15, 20, 25, 30, 35')
+    expect(countUpBy(35, 5)).to.equal('5, 10, 15, 20, 25, 30, 35');
+  });
+
+  it('will handle string input and try to parseInt', function() {
+    expect(countUpBy('30', '5')).to.equal('5, 10, 15, 20, 25, 30');
   });
 });
