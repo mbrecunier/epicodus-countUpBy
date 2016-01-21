574195ed364121b91e9b7e8d13d828744f9e5206
Nicholas Jensen-Hay <nicholasjensenhay@gmail.com>
2016-01-21 09:06:19 -0800

    add reversed input functionality to countUpBy

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
 var countUpBy = function(countBy, countTo) {
   var output = [];
+  if (countBy > countTo) {
+    countBy = [countTo, countTo = countBy][0];
+  }
   for (var index = countBy; index <= countTo; index += countBy) {
     if(index<=countTo) {
       output.push(index);
