862b347139370e520bd278dd892bc40d7f4cccc3
Nicholas Jensen-Hay <nicholasjensenhay@gmail.com>
2016-01-21 08:59:36 -0800

    write spec for reversing input order

--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,8 @@ describe('countUpBy', function() {
   it('counts up by a number provided to another number provided', function() {
     expect(countUpBy(5, 30)).to.equal('5, 10, 15, 20, 25, 30')
   });
+
+  it('will always count to the large input by the smaller input', function() {
+    expect(countUpBy(35, 5)).to.equal('5, 10, 15, 20, 25, 30, 35')
+  });
 });
