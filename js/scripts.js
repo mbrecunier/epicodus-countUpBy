4900bad29978b339a252ad1a90c44668d777f924
Nicholas Jensen-Hay <nicholasjensenhay@gmail.com>
2016-01-21 09:47:44 -0800

    make countUpBy handle two negative numbers

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,20 @@ var countUpBy = function(countBy, countTo) {
   var output = [];
   countBy = parseInt(countBy);
   countTo = parseInt(countTo);
-  if (countBy > countTo) {
-    countBy = [countTo, countTo = countBy][0];
-  }
-  for (var index = countBy; index <= countTo; index += countBy) {
-    if(index<=countTo) {
-      output.push(index);
+  if(countBy > 0 && countTo > 0) {
+    if (countBy > countTo) {
+      countBy = [countTo, countTo = countBy][0];
+    }
+    for (var index = countBy; index <= countTo; index += countBy) {
+      if(index<=countTo) {
+        output.push(index);
+      }
+    }
+  } else {
+    for (var index = countBy; index >= countTo; index += countBy) {
+      if(index>=countTo) {
+        output.push(index);
+      }
     }
   }
   return output.join(', ');
